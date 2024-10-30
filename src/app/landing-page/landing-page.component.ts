a35b6eabd3fdc4f08516dddf40d563c72994f52e
Abhishek Kumar <272002abhishek@gmail.com>
2024-10-31 00:25:06 +0530

    Added call to action feature

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+  email: string = '';
+
+  subscribe() {
+    
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailPattern.test(this.email)) {
+      alert("Your email has been added." + this.email);
+      this.email = ''; // Clear the input after subscription
+    } else {
+      alert("Please enter a valid email address." );
+    }
+  }
 
 }
